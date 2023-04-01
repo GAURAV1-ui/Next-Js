ec535bac8c0700210855a9f9b7ff8da7fc0b699e
GAURAV <iamgaurav201@gmail.com>
2023-04-01 23:59:11 +0530

    new id added

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,4 +1,11 @@
+import { useRouter } from "next/router";
+
 function News (){
+
+    const router = useRouter();
+
+    const newId = router.query.newsId;
+
     return <h1>The News pages</h1>
 }
 
---
+++ pages/news/index.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+function NewsPage (){
+    return
+        <Fragment>
+            <h1>The News page</h1>
+            <ul>
+                <li>NextJS is a ...</li>
+                <li>Something else</li>
+            </ul>
+        </Fragment> 
+}
+
+export default NewsPage;
